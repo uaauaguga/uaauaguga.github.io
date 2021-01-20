4c214bdd54674ed29656a70f96cdd54de4e78f3e
uaauaguga <jinyf1998@163.com>
2021-01-20 16:01:28 +0800

    Add notes

---
+++ notes/prepare-reference-data.sh
@@ -0,0 +1,5 @@
+## Notes for benchmark data preparation
+- Split Rfam seed alignments
+```{bash}
+scripts/separate-seed.py -i data/Rfam/seed-alignments-concatenated/Rfam.seed -o data/Rfam/seed-alignments
+```
--- scripts/separate-seed.py
+++ scripts/separate-seed.py
@@ -2,9 +2,9 @@
 from tqdm import tqdm
 import argparse
 
-parser = argparse.ArgumentParser(description='Convert stockholm format to dot bracket format.')
-parser.add_argument('--input', '-i',required=True,help="Input path, read from stdin by default.")
-parser.add_argument('--outdir','-o',required=True,help="Output path, print to stdout by default.")
+parser = argparse.ArgumentParser(description='Split Rfam seed alignments, each RNA family as a stockholm format file.')
+parser.add_argument('--input', '-i',required=True,help="Input path")
+parser.add_argument('--outdir','-o',required=True,help="Output path")
 args = parser.parse_args()
 
 
@@ -17,7 +17,7 @@ with open(inSeed,encoding="ISO-8859-1") as f:
         if line.startswith('# STOCKHOLM 1.0'):
             if content:
                 content = ''.join(content)
-                with open(outDir+"/{}.seed".format(accession),"w") as f:
+                with open(outDir+"/{}.stk".format(accession),"w") as f:
                     f.write(content)
             content = []
         elif line.startswith('#=GF AC'):
